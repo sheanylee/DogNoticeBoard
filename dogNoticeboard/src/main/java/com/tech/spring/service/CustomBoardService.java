653b92157cd512de5827f8a931f90d3ed4ad0b82
sisi0401 <sisi0401@hanmail.net>
2023-03-15 14:07:01 +0900

    최종

---
+++ dogNoticeboard/src/main/java/com/tech/spring/service/CustomBoardService.java
@@ -0,0 +1,47 @@
+package com.tech.spring.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.tech.spring.dto.BoardDto;
+import com.tech.spring.vopage.PageVO;
+import com.tech.spring.vopage.PageVO_review;
+
+public interface CustomBoardService {
+
+	//게시판 글개수
+	int countBoard(Map<String, String> map);
+
+	//게시판 등록
+	int write(BoardDto dto);
+
+	//게시판 상세
+	BoardDto detail(int board_seq);
+	
+	//게시판 삭제
+	int remove(String board_seq);
+
+	//게시판 수정 페이지
+	BoardDto editPage(int board_seq);
+
+	//게시판 수정
+	int edit(BoardDto dto);
+
+	//게시판 목록
+	List<BoardDto> board(PageVO pageInfo, Map<String, String> map);
+
+	//게시판 리뷰 목록
+	List<BoardDto> review(PageVO_review pageInfo, int board_seq);
+
+	//게시판 리뷰 등록
+	int writeReview(BoardDto dto);
+
+	//게시판 리뷰 삭제
+	int deleteReview(int review_seq);
+
+	//게시판 리뷰 개수
+	int countReview(int board_seq);
+
+}
+
+
